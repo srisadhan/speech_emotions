c608f7861bd8b16fe9baaf9a3e9caa0195e42183
amsterg <amsterg@github.com>
2021-05-11 16:07:12 -0400

    add speaker encoder and its req utils

---
+++ src/datasets/data_utils.py
@@ -0,0 +1,370 @@
+import matplotlib
+from random import randint, sample
+import torch
+from torch.utils import data
+import matplotlib.pyplot as plt
+import h5py
+import warnings
+from tqdm import tqdm
+from colorama import Fore
+import pathlib
+from scipy.ndimage import binary_dilation
+import soundfile
+import webrtcvad
+import numpy as np
+from librosa.display import specshow
+import librosa
+from collections import Counter
+import pandas as pd
+from yaml import safe_load
+import sys
+import os
+from sklearn.metrics import confusion_matrix
+from itertools import product
+import json
+
+# nopep8
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.getcwd())
+
+
+warnings.filterwarnings("ignore")
+
+with open('src/config.yaml', 'r') as f:
+    config = safe_load(f.read())
+
+RAW_DATA_DIR = config['raw_audio_data']
+PROC_DATA_DIR = config['proc_data_dir']
+INTERIM_DATA_DIR = config['interim_data_dir']
+MODEL_SAVE_DIR = config['model_save_dir']
+VIS_DIR = config['vis_dir']
+RUNS_DIR = config['runs_dir']
+
+SAMPLING_RATE = config['resampling_rate']
+
+N_FFT = config['n_fft']
+H_L = config['n_fft']
+STEP_SIZE_EM = int((SAMPLING_RATE/16)/H_L)
+MEL_CHANNELS = config['n_mels']
+SMOOTHING_LENGTH = config['smoothing_length']
+SMOOTHING_WSIZE = config['smoothing_wsize']
+WINDOW_STEP = config['window_step']
+DBFS = config['dbfs']
+SMOOTHING_WSIZE = int(SMOOTHING_WSIZE * SAMPLING_RATE / 1000)
+
+dirs_ = set([globals()[d] for d in globals() if d.__contains__('DIR')] +
+            [config[d] for d in config if d.__contains__('DIR')])
+
+VAD = webrtcvad.Vad(mode=config['vad_mode'])
+
+
+def structure(dirs=[]):
+    """
+    Create necessary directory structure
+
+    Args:
+
+    Returns:
+
+    """
+    dirs_reqd = set(list(dirs_) + list(dirs))
+    for data_dir in dirs_reqd:
+        if not pathlib.Path.exists(pathlib.Path(data_dir)):
+            os.makedirs(data_dir)
+
+
+def normalization(aud, norm_type='peak'):
+    """
+    Normalize the given audio clip
+
+    Args:
+
+    Returns:
+
+    """
+    try:
+        assert len(aud) > 0
+        if norm_type == "peak":
+            aud = aud / np.max(aud)
+
+        elif norm_type == "rms":
+            dbfs_diff = DBFS - (20 *
+                                np.log10(np.sqrt(np.mean(np.square(aud)))))
+            if DBFS > 0:
+                aud = aud * np.power(10, dbfs_diff / 20)
+
+        return aud
+    except AssertionError as e:
+        raise AssertionError("Empty audio sig")
+
+
+def detect_voices(aud, sr=44100):
+    """
+    Detect the presence and absence of voices in an array of audio 
+
+    Args:
+
+    Returns:
+
+    """
+    pcm_16 = np.round(
+        (np.iinfo(np.int16).max * aud)).astype(np.int16).tobytes()
+    voices = [
+        VAD.is_speech(pcm_16[2 * ix:2 * (ix + SMOOTHING_WSIZE)],
+                      sample_rate=SAMPLING_RATE)
+        for ix in range(0, len(aud), SMOOTHING_WSIZE)
+    ]
+    return voices
+
+
+def preprocess_aud(aud_input, sr=44100):
+    """
+    Preprocess and return the provided audio
+
+    Args:
+
+    Returns:
+
+    """
+    if isinstance(aud_input, list) or isinstance(aud_input, np.ndarray):
+        aud = np.array(aud_input)
+    else:
+        fname = aud_input
+        aud, sr = librosa.load(fname, sr=None)
+    if sr != SAMPLING_RATE:
+        aud = librosa.resample(aud, sr, SAMPLING_RATE)
+    try:
+        aud = normalization(aud, norm_type='peak')
+
+    except AssertionError as e:
+        print(AssertionError("Empty audio sig"))
+
+    trim_len = len(aud) % SMOOTHING_WSIZE
+    aud = np.append(aud, np.zeros(SMOOTHING_WSIZE - trim_len))
+
+    assert len(aud) % SMOOTHING_WSIZE == 0, print(len(aud) % trim_len, aud)
+
+    # pcm_16 = np.round(
+    #     (np.iinfo(np.int16).max * aud)).astype(np.int16).tobytes()
+    # voices = [
+    #     VAD.is_speech(pcm_16[2 * ix:2 * (ix + SMOOTHING_WSIZE)],
+    #                   sample_rate=SAMPLING_RATE)
+    #     for ix in range(0, len(aud), SMOOTHING_WSIZE)
+    # ]
+    voices = detect_voices(aud, SAMPLING_RATE)
+
+    smoothing_mask = np.repeat(
+        binary_dilation(voices, np.ones(SMOOTHING_LENGTH)), SMOOTHING_WSIZE)
+    # print(len(smoothing_mask))
+    aud = aud[smoothing_mask]
+
+    try:
+        aud = normalization(aud, norm_type='peak')
+        return aud, SAMPLING_RATE
+
+    except AssertionError as e:
+        print(AssertionError("Empty audio sig"))
+        return aud, SAMPLING_RATE
+        # exit()
+
+
+def mel_spectogram(aud):
+    """
+    Create a mel-spectogram of the given audio
+
+    Args:
+
+    Returns:
+
+    """
+    mel = librosa.feature.melspectrogram(aud,
+                                         sr=SAMPLING_RATE,
+                                         n_fft=N_FFT,
+                                         hop_length=H_L,
+                                         n_mels=MEL_CHANNELS)
+    # mel = np.log(mel + 1e-5)
+    return mel
+
+
+def split_audio_ixs(n_samples, rate=STEP_SIZE_EM, min_coverage=0.75):
+    """
+    Create audio,mel slice indices for the audio clip 
+
+    Args:
+
+    Returns:
+
+    """
+    assert 0 < min_coverage <= 1
+
+    # Compute how many frames separate two partial utterances
+    samples_per_frame = int((SAMPLING_RATE * WINDOW_STEP / 1000))
+    n_frames = int(np.ceil((n_samples + 1) / samples_per_frame))
+    frame_step = int(np.round((SAMPLING_RATE / rate) / samples_per_frame))
+    assert 0 < frame_step, "The rate is too high"
+    assert frame_step <= H_L, "The rate is too low, it should be %f at least" % \
+        (SAMPLING_RATE / (samples_per_frame * H_L))
+
+    wav_slices, mel_slices = [], []
+    steps = max(1, n_frames - H_L + frame_step + 1)
+    for i in range(0, steps, frame_step):
+        mel_range = np.array([i, i + H_L])
+        wav_range = mel_range * samples_per_frame
+        mel_slices.append(slice(*mel_range))
+        wav_slices.append(slice(*wav_range))
+
+    last_wav_range = wav_slices[-1]
+    coverage = (n_samples - last_wav_range.start) / \
+        (last_wav_range.stop - last_wav_range.start)
+    if coverage < min_coverage and len(mel_slices) > 1:
+        mel_slices = mel_slices[:-1]
+        wav_slices = wav_slices[:-1]
+    return wav_slices, mel_slices
+
+
+def plot_confusion_matrix(preds, labels, label_names=None, normalize='true'):
+    """
+    Summary:
+
+    Args:
+
+    Returns:
+
+    """
+    cm = confusion_matrix(preds, labels, normalize=normalize)
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    tick_marks = np.arange(len(label_names))
+    cmap_min, cmap_max = plt.cm.Blues(0), plt.cm.Blues(256)
+
+    plt.xticks(tick_marks, label_names, rotation=45)
+    plt.yticks(tick_marks, label_names)
+    plt.title("Ad-Detection Confusion Matrix")
+    plt.tight_layout()
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    if bool(normalize.capitalize()):
+        fmt = '.2f'
+    else:
+        fmt = 'd'
+    thresh = (cm.max() + cm.min()) / 2.0
+    for i, j in product(range(len(label_names)), range(len(label_names))):
+        color = cmap_max if cm[i, j] < thresh else cmap_min
+        plt.text(i, j, format(cm[i, j], fmt),
+                 horizontalalignment="center", color=color)
+    plt.show()
+
+
+class HDF5TorchDataset(data.Dataset):
+    """
+    Class for creating an Torch dataset based with random mel selection form HDF5 file
+
+
+    Attributes
+    ----------
+    ...
+
+    Methods
+    -------
+    ...
+
+    """
+
+    def __init__(self, data, device=torch.device('cpu')):
+        hdf5_file = os.path.join(PROC_DATA_DIR, '{}.hdf5'.format(data))
+        self.hdf5_file = h5py.File(hdf5_file, 'r')
+        self.accents = self.hdf5_file.keys()
+        with open('src/config.yaml', 'r') as f:
+            self.config = safe_load(f.read())
+        self.device = device
+
+    def __len__(self):
+        return len(self.accents) + int(1e4)
+
+    def _get_acc_uttrs(self):
+        while True:
+            rand_accent = sample(list(self.accents), 1)[0]
+            if self.hdf5_file[rand_accent].__len__() > 0:
+                break
+        wavs = list(self.hdf5_file[rand_accent])
+        while len(wavs) < self.config['uttr_count']:
+            wavs.extend(sample(wavs, 1))
+
+        rand_wavs = sample(wavs, self.config['uttr_count'])
+        rand_accent_ix = list(self.accents).index(rand_accent)
+        rand_uttrs = []
+        labels = []
+        for wav in rand_wavs:
+            wav_ = self.hdf5_file[rand_accent][wav]
+
+            rix = randint(
+                0, wav_.shape[1] - self.config['sliding_win_size'])
+
+            ruttr = wav_[:, rix:rix +
+                         self.config['sliding_win_size']]
+
+            ruttr = torch.Tensor(ruttr)
+            rand_uttrs.append(ruttr)
+            labels.append(rand_accent_ix)
+        return rand_uttrs, labels
+
+    def __getitem__(self, ix=0):
+        rand_uttrs, labels = self._get_acc_uttrs()
+        rand_uttrs = torch.stack(rand_uttrs).to(device=self.device)
+        labels = torch.LongTensor(labels).to(device=self.device)
+        return rand_uttrs, labels
+
+    def collate(self, data):
+        pass
+
+
+def load_audio(fname, sample_rate=None):
+    """
+    Load any give audio
+
+    Args:
+
+    Returns:
+
+    """
+    aud, sr = librosa.load(fname, sr=None)
+    if sample_rate is None:
+        sample_rate = config['SAMPLING_RATE']
+    aud, sr = preprocess_aud(aud, sr)
+    return aud
+
+
+def write_hdf5(out_file, data):
+    """
+   Write the provieded dict to hdf5 file
+
+    Args:
+
+    Returns:
+
+    """
+    proc_file = h5py.File(out_file, 'w')
+    print(proc_file)
+    for g in data:
+        group = proc_file.create_group(g)
+        for datum in data[g]:
+            try:
+                group.create_dataset("mel_spects_{}".format(datum[0]),
+                                     data=datum[1])
+            except Exception as e:
+                print(group.name, datum[0], e)
+
+    proc_file.close()
+    exit()
+
+
+if __name__ == "__main__":
+    structure()
+    dataset = HDF5TorchDataset(
+        config['train_dir']
+    )
+    loader = data.DataLoader(dataset, 4)
+    for y in loader:
+        print(y[0])
+        print(y[1])
+        break
---
+++ src/models/speaker_encoder.py
@@ -0,0 +1,400 @@
+import argparse
+from sklearn.metrics import roc_curve
+from scipy.interpolate import interp1d
+from scipy.optimize import brentq
+import librosa
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from math import floor
+from torch.utils.tensorboard import SummaryWriter
+from yaml import safe_load
+import os
+import sys
+
+# from src.data.data_utils import ImbalancedDatasetSampler
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.getcwd())
+from src.datasets.data_utils import H_L, STEP_SIZE_EM, split_audio_ixs, mel_spectogram, preprocess_aud, HDF5TorchDataset  # noqa
+np.random.seed(42)
+
+
+class SPEAKER_ENCODER(nn.Module):
+    """
+    Attributes
+    ----------
+    ...
+
+    Methods
+    -------
+    ...
+
+    """
+
+    def __init__(self,
+                 input_shape=(160, 40),
+                 load_model=False,
+                 epoch=0,
+                 dataset_train='',
+                 dataset_val='',
+                 device=torch.device('cpu'),
+                 loss_=None, mode='train'):
+        super(SPEAKER_ENCODER, self).__init__()
+        self.input_shape = input_shape
+        with open('src/config.yaml', 'r') as f:
+            self.config_yml = safe_load(f.read())
+
+        model_save_dir = os.path.join(self.config_yml['model_save_dir'],
+                                      'SPEAKER_ENCODER')
+        if not os.path.exists(model_save_dir):
+            os.makedirs(model_save_dir)
+
+        self.model_save_string = os.path.join(
+            model_save_dir, self.__class__.__name__ + '_Epoch_{}.pt')
+
+        log_dir = os.path.join(
+            self.config_yml['runs_dir'],
+            '{}_{}'.format(dataset_train, self.__class__.__name__))
+        self.writer = SummaryWriter(log_dir=os.path.join(
+            log_dir, "run_{}".format(
+                len(os.listdir(log_dir)) if os.path.exists(log_dir) else 0)))
+
+        self.device = device
+
+        if mode == 'train':
+            self.dataset_train = HDF5TorchDataset(dataset_train, device=device)
+            if dataset_val == '':
+                self.dataset_val = self.dataset_train
+            else:
+                self.dataset_val = HDF5TorchDataset(
+                    args.dataset_val, device=device)
+
+        self.lstm = nn.LSTM(self.config_yml['n_mels'],
+                            self.config_yml['hidden_size'],
+                            self.config_yml['num_layers'],
+                            batch_first=True,
+                            bidirectional=False)
+        self.linear = nn.Linear(1*self.config_yml['hidden_size'],
+                                self.config_yml['embedding_size'])
+
+        self.similarity_weight = torch.nn.Parameter(
+            torch.Tensor([10.0]), requires_grad=True)
+        self.similarity_bias = torch.nn.Parameter(
+            torch.Tensor([-5.0]), requires_grad=True)
+
+        self.dropout = nn.Dropout()
+        self.relu = nn.ReLU()
+        self.softmax = torch.nn.Softmax()
+        self.load_model = load_model
+        self.epoch = epoch
+        self.loss_ = loss_
+        self.opt = None
+        self.ce_loss = nn.CrossEntropyLoss(reduction='mean')
+
+    def forward(self, frames):
+
+        _, (x, _) = self.lstm(frames)  # lstm out,hidden,
+        # x = x[:, -1]  #last layer -> embeds
+
+        x = self.linear(x[-1])
+        x = self.relu(x)
+        # x = torch.mean(x,dim=1)
+        # x = self.relu(x)
+
+        x = x * torch.reciprocal(torch.norm(x, dim=1, keepdim=True))
+
+        return x
+
+    def loss_fn(self, loss_, embeds, labels):
+
+        lang_count = int(self.config_yml['batch_size'] /
+                         self.config_yml['uttr_count'])
+        embeds3d = embeds.view(lang_count, self.config_yml['uttr_count'], -1)
+        dcl = self.direct_classification_loss(embeds, labels)
+
+        centroids = torch.mean(embeds3d, dim=1)
+
+        centroids_neg = (torch.sum(embeds3d, dim=1, keepdim=True) -
+                         embeds3d) / (self.config_yml['uttr_count'] - 1)
+        cosim_neg = torch.cosine_similarity(embeds,
+                                            centroids_neg.view_as(embeds),
+                                            dim=1).view(lang_count, -1)
+        centroids = centroids.repeat(
+            lang_count * self.config_yml['uttr_count'], 1)
+
+        embeds2de = embeds.unsqueeze(1).repeat_interleave(lang_count, 1).view(
+            -1, self.config_yml['embedding_size'])
+        cosim = torch.cosine_similarity(embeds2de, centroids)
+        cosim_matrix = cosim.view(lang_count, self.config_yml['uttr_count'],
+                                  -1)
+        neg_ix = list(range(lang_count))
+
+        cosim_matrix[neg_ix, :, neg_ix] = cosim_neg
+
+        sim_matrix = (self.similarity_weight * cosim_matrix) + \
+            self.similarity_bias
+
+        sim_matrix = sim_matrix.view(self.config_yml['batch_size'], -1)
+        targets = torch.range(
+            0, self.config_yml['acc_count'] - 1).repeat_interleave(
+                self.config_yml['uttr_count']).long().to(device=self.device)
+
+        ce_loss = loss_(sim_matrix, targets)
+
+        return (ce_loss, dcl), sim_matrix
+
+    def direct_classification_loss(self, embeds, labels):
+        labels = labels.reshape(-1, 1).squeeze()
+        return self.ce_loss(embeds, labels)
+
+    def train_loop(self,
+                   opt,
+                   lr_scheduler,
+                   loss_,
+                   batch_size=1,
+                   gaze_pred=None,
+                   cpt=0):
+
+        train_iterator = torch.utils.data.DataLoader(self.dataset_train,
+                                                     batch_size=batch_size,
+                                                     shuffle=True,
+                                                     drop_last=True)
+
+        self.val_iterator = torch.utils.data.DataLoader(self.dataset_val,
+                                                        batch_size=batch_size,
+                                                        shuffle=True,
+                                                        drop_last=True)
+
+        if self.load_model:
+            self.load_model_cpt(cpt=cpt)
+
+        for epoch in range(self.epoch, 20000):
+            for i, (data, labels) in enumerate(train_iterator):
+                data = data.view(
+                    -1,
+                    self.config_yml['MEL_CHANNELS'],
+                    self.config_yml['sliding_win_size'],
+                ).transpose(1, 2)
+
+                opt.zero_grad()
+
+                embeds = self.forward(data)
+                (ce_loss,
+                 dcl), sim_matrix = self.loss_fn(loss_, embeds, labels)
+                self.loss = ce_loss + dcl
+
+                self.loss.backward()
+
+                torch.nn.utils.clip_grad_norm_(self.parameters(), 3.0)
+
+                opt.step()
+                self.writer.add_scalar('ce_loss', ce_loss.data.item(), epoch)
+                self.writer.add_scalar('dcl', dcl.data.item(), epoch)
+                self.writer.add_scalar('Loss', self.loss.data.item(), epoch)
+                # self.writer.add_scalar('ValLoss', self.val_loss(), epoch)
+
+                self.writer.add_scalar('EER', self.eer(sim_matrix), epoch)
+
+            if epoch % 1 == 0:
+                # self.writer.add_scalar('Loss', loss.data.item(), epoch)
+                # self.writer.add_scalar('Val Loss', self.val_loss(), epoch)
+                # self.writer.add_scalar('EER', self.eer(sim_matrix), epoch)
+                # self.writer.add_histogram('sim', sim_matrix, epoch)
+
+                torch.save(
+                    {
+                        'epoch': epoch,
+                        'model_state_dict': self.state_dict(),
+                        'optimizer_state_dict': opt.state_dict(),
+                        'loss': self.loss,
+                    }, self.model_save_string.format(epoch))
+
+    def embed(self, aud, group=True):
+
+        aud_splits, mel_splits = split_audio_ixs(len(aud))
+        max_aud_length = aud_splits[-1].stop
+
+        if max_aud_length >= len(aud):
+            aud = np.pad(aud, (0, max_aud_length - len(aud)), "constant")
+
+        mel = mel_spectogram(aud).astype(np.float32).T
+        mels = np.array([mel[s] for s in mel_splits[0:1]])
+
+        mels = torch.from_numpy(mels).to(self.device)
+        embeds_all = []
+        with torch.no_grad():
+            for i in range(0, mels.shape[0], 64):
+                embeds = self.forward(
+                    mels[i:min(i+64, mels.shape[0]), :, :].to(self.device))
+                embeds = embeds / torch.norm(embeds, dim=1, keepdim=True)
+                embeds_all.append(embeds)
+        embeds = torch.cat(embeds_all)
+
+        if group:
+            embeds = torch.mean(embeds, dim=0)
+            embeds = embeds / torch.norm(embeds)
+
+        embeds = embeds.cpu().data.numpy()
+        # self.train()
+
+        return embeds, (aud_splits, mel_splits)
+
+    def accuracy(self):
+        acc = 0
+        ix = 0
+        for i, data in enumerate(self.val_data):
+            uttrs = data[0]
+            embeds = self.forward(uttrs)
+            # TODO
+        return (acc / ix)
+
+    def eer(self, sim_matrix=None):
+        with torch.no_grad():
+
+            targets = F.one_hot(
+                torch.arange(0, self.config_yml['acc_count']),
+                num_classes=self.config_yml['acc_count']).repeat_interleave(
+                    self.config_yml['uttr_count'], 1).long().T
+
+            fpr, tpr, thresholds = roc_curve(
+                targets.flatten(),
+                sim_matrix.detach().flatten().cpu().numpy())
+
+            eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+
+        return eer
+
+    def val_loss(self):
+        with torch.no_grad():
+            val_loss = []
+
+            for ix, (datum, labels) in enumerate(self.val_iterator):
+                datum = datum.view(
+                    -1,
+                    self.config_yml['MEL_CHANNELS'],
+                    self.config_yml['sliding_win_size'],
+                ).transpose(1, 2)
+                embeds = self.forward(datum)
+
+                (ce_loss,
+                 dcl), sim_matrix = self.loss_fn(self.loss_, embeds, labels)
+                loss = ce_loss + dcl
+
+                val_loss.append(loss)
+
+                if ix == self.config_yml['VAL_LOSS_COUNT']:
+                    break
+
+        return torch.mean(torch.stack(val_loss)).data.item()
+
+    def load_model_cpt(self, cpt=0, opt=None, device=torch.device('cuda')):
+        self.epoch = cpt
+
+        model_pickle = torch.load(self.model_save_string.format(self.epoch),
+                                  map_location=device)
+        self.load_state_dict(model_pickle['model_state'])
+        if opt:
+            self.opt.load_state_dict(model_pickle['optimizer_state'])
+        self.global_step = model_pickle['step']
+        # self.loss = model_pickle['loss']
+        self.loss = None
+        print("Loaded Model at epoch {},with loss {}".format(
+            self.epoch, self.loss))
+
+    def infer(self, fname, cpt=None):
+        aud = preprocess(fname)
+        embeds = self.embed(aud, group=True)
+        return embeds
+
+    def similarity(self, embed1, embed2):
+        sim = torch.cosine_similarity(
+            torch.tensor(embed1).unsqueeze(0),
+            torch.tensor(embed2).unsqueeze(0)).data.item()
+        # sim = embed1 @ embed2
+        return sim
+
+    def sim_matrix_infer(self, fnames, cpt):
+        sim_matrix = np.zeros((len(fnames), len(fnames)))
+        for i, f1 in enumerate(fnames):
+            for j, f2 in enumerate(fnames):
+                sim_matrix[i, j] = self.similarity(self.infer(f1, cpt),
+                                                   self.infer(f2, cpt))
+
+        return sim_matrix
+
+    def diarize(self, aud, ref_embed):
+        embed = self.embed(aud, group=True)
+        sim = embed@ref_embed
+        return sim
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device",
+                        help="cpu or cuda",
+                        default='cuda',
+                        choices=['cpu', 'cuda'])
+    parser.add_argument("--dataset_train",
+                        help="path to train_dataset",
+                        required=False, default='timit_4_train')
+    parser.add_argument("--dataset_val",
+                        help="path to val_dataset",
+                        required=False, default='timit_4_train')
+    parser.add_argument("--mode",
+                        help="train or eval",
+                        required=True,
+                        choices=['train', 'eval'])
+    parser.add_argument(
+        "--filedir",
+        help="dir with fnames to run similiarity eval,atleast 2, separted by a comma",
+        type=str)
+    parser.add_argument("--load_model",
+                        help="to load previously saved model checkpoint",
+                        default=False)
+    parser.add_argument(
+        "--cpt",
+        help="# of the save model cpt to load, only valid if valid_cpt is true"
+    )
+
+    args = parser.parse_args()
+
+    device = torch.device(args.device)
+    loss_ = torch.nn.CrossEntropyLoss(reduction='sum')
+    encoder = SPEAKER_ENCODER(dataset_train=args.dataset_train,
+                              dataset_val=args.dataset_val,
+                              device=device,
+                              loss_=loss_,
+                              load_model=args.load_model, mode=args.mode).to(device=device)
+    optimizer = torch.optim.SGD(encoder.parameters(), lr=1e-2)
+
+    encoder.opt = optimizer
+    cpt = args.cpt
+    if args.load_model:
+        encoder.load_model_cpt(cpt=cpt, device=device)
+    lr_scheduler = None
+    if args.mode == 'train':
+        encoder.train_loop(optimizer,
+                           lr_scheduler,
+                           loss_,
+                           batch_size=encoder.config_yml['acc_count'],
+                           cpt=cpt)
+    elif args.mode == 'vis':
+
+        fnames = [
+            os.path.join(args.filedir, x)
+            for x in sorted(os.listdir(args.filedir))
+        ]
+
+        sim_matrix = encoder.sim_matrix_infer(fnames, cpt)
+    elif args.mode == 'eval':
+        aud1, _ = preprocess_aud(os.path.join(
+            encoder.config_yml['vis_dir'], '1.wav'))
+        aud2, _ = preprocess_aud(os.path.join(
+            encoder.config_yml['vis_dir'], '1.wav'))
+
+        embed1, _ = encoder.embed(aud1, group=True)
+        embed2, _ = encoder.embed(aud2, group=True)
+
+        print(embed1@embed2)
